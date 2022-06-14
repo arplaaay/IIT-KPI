826a686405329e07118157b3406b4b391386f3a1
Artem Bystritskyi <artemb0607@gmail.com>
2022-06-14 17:21:49 +0300

    add bot

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Telegraf } = require('telegraf')
+
+let range = 1000
+const bot = new Telegraf("Telegram bot token")
+bot.start((ctx) => ctx.reply('1000-7?'))
+bot.hears(`sf banned`, (ctx) => {
+    ctx.reply(`${range}`)
+    range = range - 77
+})
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
