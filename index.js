2e37907e99dc0a03235e7299e8964ca36deca774
Artem Bystritskyi <86619106+arplaaay@users.noreply.github.com>
2022-06-14 17:39:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 
 let range = 707
-const bot = new Telegraf("Telegram bot token")
+const bot = new Telegraf("5575133769:AAHUBndE6ZwERJxocQY2_4VT5FurOpN4vMM")
 bot.start((ctx) => ctx.reply('1000-7?'))
 bot.hears(`sf banned`, (ctx) => {
     ctx.reply(`${range}`)
