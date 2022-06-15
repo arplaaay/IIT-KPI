4bd1316488f8d18f960e3025e806ec00190009f8
Artem Bystritskyi <86619106+arplaaay@users.noreply.github.com>
2022-06-15 11:00:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { Telegraf } = require('telegraf')
 
 let range = 707
 const bot = new Telegraf("5575133769:AAHUBndE6ZwERJxocQY2_4VT5FurOpN4vMM")
-bot.start((ctx) => ctx.reply('1000-7?'))
-bot.hears(`sf banned`, (ctx) => {
+bot.start((ctx) => ctx.reply('hello world!'))
+bot.hears(`da privet`, (ctx) => {
     ctx.reply(`${range}`)
     range = range - 77
 })
