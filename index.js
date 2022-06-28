5b6027dd68d31a81f1194d530170e6629b5cc942
Artem Bystritskyi <86619106+arplaaay@users.noreply.github.com>
2022-06-28 11:57:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf')
 
-let range = 600
+let range = 800
 const bot = new Telegraf("5575133769:AAHUBndE6ZwERJxocQY2_4VT5FurOpN4vMM")
 bot.start((ctx) => ctx.reply('hello world!'))
 bot.hears(`da privet`, (ctx) => {
